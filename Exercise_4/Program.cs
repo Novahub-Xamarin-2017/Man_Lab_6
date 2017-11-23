be398e6aca47f5ffc66e93852f10b8277a9c6b16
Mẫn <vanman.lqd@gmail.com>
2017-11-23 22:03:42 +0700

    Add Exercise_4

---
+++ Exercise_4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+namespace Exercise_4
+{
+    class Program
+    {
+        static List<string> FilterFile(string root, Predicate<string> predicate)
+        {
+            var files = new List<string>();
+            
+            foreach (var file in Directory.GetFiles(root))
+            {
+                if (predicate(file))
+                {
+                    files.Add(file);
+                }
+            }
+            return files;
+        }
+        static void Main(string[] args)
+        {
+            FilterFile(@"D:\Tumblers", x => x.EndsWith("")).ForEach(Console.WriteLine);
+            Console.ReadKey();
+        }
+    }
+}
